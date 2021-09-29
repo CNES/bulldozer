f09d14b705d29843ab084045ea36189b4e466e33
Dimitri Lallement <dimitri.lallement@cnes.fr>
2021-09-29 09:30:36 +0000

    [add] - Setup logger

--- bulldozer/bulldozer_pipeline.py
+++ bulldozer/bulldozer_pipeline.py
@@ -1,12 +1,19 @@
+import os
 import sys
 import logging
+import logging.config
+from shutil import copy
+from datetime import datetime
+from sys import stdout
 from bulldozer.core.dsm_preprocess import preprocess
 from bulldozer.core.dtm_extraction import run
 from bulldozer.core.dtm_postprocess import postprocess
 from bulldozer.utils.config_parser import ConfigParser
 from bulldozer.utils.helper import init_logger
+
+logging.config.fileConfig(os.path.join(os.path.dirname(os.path.abspath(__file__)), "../conf/logging.ini"), 
+                          disable_existing_loggers=False)
 logger = logging.getLogger(__name__)
-# No data value constant used in bulldozer
 
 class Pipeline:
     def __init__(self, config_path : str, verbosity = False):
@@ -14,6 +21,8 @@ class Pipeline:
 
         """
         init_logger(logger)
+        starting_time = datetime.now()
+        logger.info("Starting time: " + starting_time.strftime("%Y-%m-%d %H:%M:%S"))
         parser = ConfigParser(verbosity)
         cfg = parser.read(config_path)
 
@@ -32,7 +41,18 @@ class Pipeline:
         quality_mask_path = cfg['outputDir'] + 'quality_mask.tif'
         postprocess(dtm_path, cfg['outputDir'], quality_mask_path, 
                     cfg['dhm'], cfg['dsmPath'])
-    
+
+        logger.info("Ending time: {} (Runtime: {}s)"
+                    .format(datetime.now().strftime("%Y-%m-%d %H:%M:%S"), 
+                    datetime.now()-starting_time))
+        
+        if cfg['keepLog']:
+            try:
+                copy(os.path.join(os.path.dirname(os.path.abspath(__file__)), 
+                    "../logs/bulldozer_logfile.log"),
+                    os.path.dirname(os.path.abspath(cfg['outputDir'] + "bulldozer_logfile.log")))
+            except Exception as e:
+                logger.warning("Error while writting the logfile: " + str(e), exc_info=True)
 
 if __name__ == "__main__":
     config_path = sys.argv[1]
